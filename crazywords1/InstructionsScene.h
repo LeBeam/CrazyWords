cd68fc1eadb93e7f6ada7c73b7b180c80734b346
franciscozm <fran4221>
2014-04-28 01:23:20 -0600

    Arreglos en el menú y agregada escena de Instrucciones

---
+++ crazywords1/InstructionsScene.h
@@ -0,0 +1,27 @@
+
+#ifndef InstructionsScene_h
+#define InstructionsScene_h
+
+#include "cocos2d.h"
+
+class InstructionsScene : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+
+    /// Se invoca cuando el jugador presiona el boton 'regresar al inicio'
+    void returnGameMenu(cocos2d::Ref* pSender);
+
+    // implement the "static create()" method manually
+    CREATE_FUNC(InstructionsScene);
+
+private:
+	/// Crea las opciones del menu, como el icono para regresar al menu del juego
+	void createMenu();
+};
+
+#endif // InstructionsScene
---
+++ crazywords1/proj.win32/InstructionsScene.cpp
@@ -0,0 +1,118 @@
+
+#include "InstructionsScene.h"
+#include <SimpleAudioEngine.h>
+
+USING_NS_CC;
+
+Scene* InstructionsScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+
+    // 'layer' is an autorelease object
+    auto layer = InstructionsScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool InstructionsScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Point origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+
+    auto label = LabelTTF::create(" ", "Arial", 24);
+
+    // position the label on the center of the screen
+    label->setPosition(Point(origin.x + visibleSize.width * 0.5f, origin.y + visibleSize.height * .64f));
+
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+
+    // Crear el fondo del menu del juego
+    auto sprite = Sprite::create("Instructions_menu/bg_instructions.png");
+
+    // position the sprite on the center of the screen
+    sprite->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+	
+	createGameTitle();
+	createMenu();
+    
+	// Reproducir la musica de la seleccion del nivel: quiza sea mejor dejar la misma del menu
+	// principal y cambiarla cuando se haya iniciado el nivel. Se deja aqui por propositos
+	// ilustrativos
+	auto sound = CocosDenshion::SimpleAudioEngine::getInstance();
+	sound->stopBackgroundMusic();
+	sound->playBackgroundMusic("Music/Game_general_music.mp3", true);
+
+    return true;
+}
+
+void InstructionsScene::createGameTitle()
+{
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Point origin = Director::getInstance()->getVisibleOrigin();
+
+    // Colocar la imagen estatica con el titulo o nombre del juego en la pantalla
+    auto gameTitle = Sprite::create("Instructions_menu/instructions_title0.png");
+    gameTitle->setPosition(Point(visibleSize.width/2 * 1 + origin.x, visibleSize.height - gameTitle->getContentSize().height +30.0f));
+    this->addChild(gameTitle, 0);
+
+	auto animation = Animation::create();
+	for ( int i = 0; i < 12; ++i )
+		animation->addSpriteFrameWithFile( String::createWithFormat("Instructions_menu/instructions_title%i.png", i)->getCString() );
+	
+	// Y luego regresarla a su estado inicial
+	for ( int i = 11; i >= 0; --i )
+		animation->addSpriteFrameWithFile( String::createWithFormat("Instructions_menu/instructions_title%i.png", i)->getCString() );
+	
+	// Indicarle que muestre cada una de las imagenes que la componen por 0.1333 segundos
+	animation->setDelayPerUnit( 0.1333f );
+	
+	// Lo anterior construye una unica animacion, repetirla siempre y cuando la pantalla de
+	// menu del juego este visible
+	auto repeatAnimation = RepeatForever::create( Animate::create(animation) );
+	gameTitle->runAction(repeatAnimation);
+
+}
+
+void InstructionsScene::createMenu()
+{
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Point origin = Director::getInstance()->getVisibleOrigin();
+
+	// Boton de la casita, para retornar al menu del juego, ubicado en la esquina superior izquierda
+	auto buttonHome = MenuItemImage::create("Instructions_menu/home_button.png", "Instructions_menu/home_button.png", CC_CALLBACK_1(InstructionsScene::returnGameMenu, this));
+	buttonHome->setPosition(Point(origin.x + buttonHome->getContentSize().width / 2.0f + 5.0f, origin.y + visibleSize.height - buttonHome->getContentSize().height / 2.0f - 5.0f));
+    auto buttonHomeMenu = Menu::create(buttonHome, NULL);
+    buttonHomeMenu->setPosition(Point::ZERO);
+    this->addChild(buttonHomeMenu, 2);
+}
+
+#include "HelloWorldScene.h"
+void InstructionsScene::returnGameMenu(Ref* pSender)
+{
+	auto newScene = HelloWorld::createScene();
+	Director::getInstance()->replaceScene(CCTransitionSlideInL::create(0.75f, newScene));
+}
+
