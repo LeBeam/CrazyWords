c5fbf2e44c992019b1afae93328b6d974abb9fa5
jfonseca1510 <jfonseca1510@gmail.com>
2014-05-20 23:32:52 -0600

    Prueba de actualizacion del proyecto de Android

--- crazywords/proj.android/jni/Android.mk
+++ crazywords/proj.android/jni/Android.mk
@@ -8,7 +8,9 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/InstructionsScene.cpp \
+                   ../../Classes/SettingsScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
